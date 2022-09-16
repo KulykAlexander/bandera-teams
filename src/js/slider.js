7541903d73df6dd76a554db69fbb5b40d6741e7f
KulykAlexander <kulikau@gmail.com>
2022-09-16 23:20:52 +0300

    adds ingredients-modal, slider js

---
+++ src/js/slider.js
@@ -0,0 +1,10 @@
+$('.kickstart-list').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 3,
+  dots: true,
+  // autoplay: true,
+  nextArrow: '.our-next-btn',
+  arrows: true,
+  prevArrow: '.our-prev-btn',
+});
