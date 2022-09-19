53e7581d4b489d40e35e505d0ca2dac8372d099e
KulykAlexander <44663040+KulykAlexander@users.noreply.github.com>
2022-09-19 17:23:02 +0000

    Deploying to gh-pages from @ KulykAlexander/bandera-teams@39327f0d8876fd4302ffbe9041a84e0eae3c48be 🚀

---
+++ index.9e178383.js
@@ -0,0 +1,2 @@
+$(".customer-reviews-list").slick({dots:!1,infinite:!0,speed:1e3,slidesToShow:1,slidesToScroll:1,autoplay:!0,nextArrow:".reviews-btn-next",prevArrow:".reviews-slide-pre",responsive:[{breakpoint:1200,settings:{slidesToShow:1,slidesToScroll:1,infinite:!0}},{breakpoint:768,settings:{slidesToShow:1,slidesToScroll:1}},{breakpoint:480,settings:{slidesToShow:1,slidesToScroll:1}}]});
+//# sourceMappingURL=index.9e178383.js.map
